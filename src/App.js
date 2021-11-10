e7c7e1bd60b234d55a3cdd68c2f78c93d54810eb
justjoshriley <joshriley316@gmail.com>
2021-11-10 14:54:20 -0800

    content component built

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Title from './Components/Title';
+import POPOSList from './Components/POPOSList';
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
     </div>
   );
 }
