f8b3c12b4dfde17154dbc5fe90520cdd94e9efb2
justjoshriley <joshriley316@gmail.com>
2021-11-10 21:46:15 -0800

    dynamic content and footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Title from './Components/Title';
 import POPOSList from './Components/POPOSList';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Title />
       <POPOSList />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <h3>Joshua Riley Copyright 2021</h3>
+        </div>
+    )
+}
+
+export default Footer;
